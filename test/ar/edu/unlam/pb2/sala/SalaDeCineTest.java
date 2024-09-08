4aafd6006b83f40132a44a63d0c8641ac7510f60
Dario_DELL <dariomiguel0@gmail.com>
2024-09-08 11:32:51 -0300

    Primer commit

---
+++ test/ar/edu/unlam/pb2/sala/SalaDeCineTest.java
@@ -0,0 +1,15 @@
+package ar.edu.unlam.pb2.sala;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SalaDeCineTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	
+}
